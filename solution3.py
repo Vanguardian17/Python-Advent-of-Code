46c259c45150abb790b24266fe92cdaaccf33d67
Vanguardian17 <40181370+Vanguardian17@users.noreply.github.com>
2021-12-05 17:19:55 +0100

    did challenge 3

---
+++ solution3.py
@@ -0,0 +1,19 @@
+
+def solution3():
+
+    with open("C:\\Users\\Carlv\\Downloads\\input3.txt") as f:
+        list = f.readlines()
+
+    start = [0, 0]
+    for v in list:
+        if v.startswith("f"):
+            start[0] += int(v[8])
+        if v.startswith("u"):
+            start[1] -= int(v[3])
+        if v.startswith("d"):
+            start[1] += int(v[5])
+
+    print("answer:", start[0]*start[1])
+
+
+solution3()
