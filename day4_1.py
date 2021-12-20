bacde12bfeeb196d5e8d873b241be31e2d6801d8
Vanguardian17 <40181370+Vanguardian17@users.noreply.github.com>
2021-12-20 14:23:49 +0100

    in progress: list creation complete

--- day4_1.py
+++ day4_1.py
@@ -14,13 +14,25 @@ def solution():
     # save all tables as lists of tuples with a value and a Boolean to mark
     # if it was already announced or not, and then save those lists nested in
     # listTables
+
+    count = 0
+    i = 0
     for k in range(0, len(list), ):
         tempList = list[k].split()
         tempList2 = []
+       # tempList3 = []
+
         for n in range(0, len(tempList)):
             tempList2.append((tempList[n], False))
 
-        listTables.append(tempList2)
+       # tempList3[i].append(tempList2)
+        listTables.append([])
+        listTables[i].append(tempList2)
+
+        count += 1
+        if(count == 5):
+            count = 0
+            i += 1
 
     # print(randomNumbers[:10])
 
@@ -28,15 +40,21 @@ def solution():
         for b in randomNumbers:
             for i in range(0, len(listTables)):
                 for j in range(0, len(listTables[i])):
-                    if(b == listTables[i][j][0]):
-                        listTables[i][j] = (listTables[i][j][0], True)
+                    for c in range(0, len(listTables[i][j])):
+                        if(b == listTables[i][j][c][0]):
+                            listTables[i][j][c] = (
+                                listTables[i][j][c][0], True)
 
-                        # check if the table won
-                        for j in listTables[i]:
-                            if False in j:
-                                continue
-                            else:
-                                indexWon = i
+                            # check if the table won
+                            hasFalse = False
+                            for m in listTables[i][j]:
+                                if m[1] == False:
+                                    hasFalse = True
+                                    break
+
+                            if(not hasFalse):
+                                indexWon = j
+                                print('found winner:', listTables[j])
                                 return indexWon
 
     print(findWinner())
